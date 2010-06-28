897528c6a2e07e80f904585d3d8fb7651554678e
bjosip <josip.biscan@gmail.com>
2010-06-28 20:16:59 +0200

    added and included css for tabs and dialogs

--- javascripts/wrt_bastard.js
+++ javascripts/wrt_bastard.js
@@ -2,17 +2,32 @@
 jQuery.oldAjax = jQuery.ajax;
 jQuery.ajax = function() {
 	try {
-		netscape.security.PrivilegeManager
-				.enablePrivilege('UniversalBrowserRead');
+		netscape.security.PrivilegeManager.enablePrivilege('UniversalBrowserRead');
 	} catch (e) {
 	}
 	$.oldAjax.apply($, arguments);
 };
 
-document.write('<script type="text/javascript" src="wrt_bastard/javascripts/commands.js"></script>');
-document.write('<script type="text/javascript" src="wrt_bastard/javascripts/device.js"></script>');
-document.write('<script type="text/javascript" src="wrt_bastard/javascripts/rss.js"></script>');
-document.write('<script type="text/javascript" src="wrt_bastard/javascripts/screen.js"></script>');
-document.write('<script type="text/javascript" src="wrt_bastard/javascripts/templating.js"></script>');
-document.write('<script type="text/javascript" src="wrt_bastard/javascripts/ui.js"></script>');
-document.write('<script type="text/javascript" src="wrt_bastard/javascripts/utility.js"></script>');
+// include a script file
+function includeJavaScript(src) {
+    document.write("<script type=\"text/javascript\" src=\"wrt_bastard/javascripts/" + src + ".js\"></script>");
+}
+
+// include a stylesheet
+function includeStylesheet(src) {
+    document.write("<style type=\"text/css\"> @import url(\"wrt_bastard/stylesheets/" +  src + ".css\"); </style>");
+}
+
+// include all JavaScripts
+includeJavaScript('commands');
+includeJavaScript('device');
+includeJavaScript('rss');
+includeJavaScript('screen');
+includeJavaScript('templating');
+includeJavaScript('ui');
+includeJavaScript('utility');
+includeStylesheet('wrt_bastard');
+
+// include styles
+includeStylesheet('tabs');
+includeStylesheet('dialog');
