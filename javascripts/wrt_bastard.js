3ebc25066b4bb7bdb2e73734801105ffe2c2f6e4
bjosip <josip.biscan@gmail.com>
2010-06-28 17:13:09 +0200

    popravljen path do wrt_bastard libraryja

--- javascripts/wrt_bastard.js
+++ javascripts/wrt_bastard.js
@@ -1,14 +1,18 @@
 // jquery ajax wrapper for wrt
-jQuery.oldAjax = jQuery.ajax
-jQuery.ajax = function(){
-  try { netscape.security.PrivilegeManager.enablePrivilege('UniversalBrowserRead'); } catch (e) {}
-  $.oldAjax.apply($, arguments);
+jQuery.oldAjax = jQuery.ajax;
+jQuery.ajax = function() {
+	try {
+		netscape.security.PrivilegeManager
+				.enablePrivilege('UniversalBrowserRead');
+	} catch (e) {
+	}
+	$.oldAjax.apply($, arguments);
 };
 
-document.write('<script type="text/javascript" src="javascript/wrt_bastard/commands.js"></script>')
-document.write('<script type="text/javascript" src="javascript/wrt_bastard/device.js"></script>')
-document.write('<script type="text/javascript" src="javascript/wrt_bastard/rss.js"></script>')
-document.write('<script type="text/javascript" src="javascript/wrt_bastard/screen.js"></script>')
-document.write('<script type="text/javascript" src="javascript/wrt_bastard/templating.js"></script>')
-document.write('<script type="text/javascript" src="javascript/wrt_bastard/ui.js"></script>')
-document.write('<script type="text/javascript" src="javascript/wrt_bastard/utility.js"></script>')
+document.write('<script type="text/javascript" src="wrt_bastard/javascripts/commands.js"></script>');
+document.write('<script type="text/javascript" src="wrt_bastard/javascripts/device.js"></script>');
+document.write('<script type="text/javascript" src="wrt_bastard/javascripts/rss.js"></script>');
+document.write('<script type="text/javascript" src="wrt_bastard/javascripts/screen.js"></script>');
+document.write('<script type="text/javascript" src="wrt_bastard/javascripts/templating.js"></script>');
+document.write('<script type="text/javascript" src="wrt_bastard/javascripts/ui.js"></script>');
+document.write('<script type="text/javascript" src="wrt_bastard/javascripts/utility.js"></script>');
