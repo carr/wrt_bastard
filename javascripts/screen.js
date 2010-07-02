f64c213b935d430fa1257aad935d04fd398dfeca
bjosip <josip.biscan@gmail.com>
2010-07-02 16:34:41 +0200

    added header button manipulation for screen

--- javascripts/screen.js
+++ javascripts/screen.js
@@ -3,7 +3,7 @@ function Screen(parent) {
   this.DEFAULT_TEMPLATE = 'screen'
   this.title = 'Screen'
   this.parent = parent
-  this.data = {}
+  this.data = []
   this.isBlocking = true
   this.showHeader = true
   this.clickItem = null
@@ -30,6 +30,7 @@ Screen.prototype.loadData = function(callback) {
 
 Screen.prototype.show = function(callback) {
   if (this.showHeader) {
+    this.resetHeaderButton()
     $('#header_text').html(this.title)
     $('#header').show()
   } else {
@@ -41,6 +42,18 @@ Screen.prototype.show = function(callback) {
 
 Screen.prototype.render = function(callback) {
   load_template(this.DEFAULT_TEMPLATE, callback)
+}
+
+Screen.prototype.resetHeaderButton = function() {
+  this.setHeaderButton('i.png', function() {
+    Utility.log('About screen')
+  })
+}
+
+Screen.prototype.setHeaderButton = function(image, callback) {
+  $('.header_button').unbind('click')
+  $('.header_button').click(callback)
+  $('.header_button img').attr('src', 'images/' + image);
 }
 
 Screen.prototype.unload = function() {
