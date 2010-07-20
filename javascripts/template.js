9f47fface4f8933aed00af93b0cbf78bf7ec6b29
carr <tomislav@infinum.hr>
2010-07-20 15:32:49 +0200

    Upgraded templating for sync (cached) method calls. Updated documentation

--- javascripts/template.js
+++ javascripts/template.js
@@ -2,6 +2,7 @@ var T = {
 	cache : {},
 	cachedTemplates : {},
 
+	// loads a template file from templates/<name>.html and returns the contents with callback(data)
 	loadTemplate : function(name, callback){
 		var that = this
 	    $.ajax({
@@ -14,6 +15,7 @@ var T = {
 	    })	
 	},
 
+	// render the 'str' template with the data from the object literal 'data'
 	renderString : function(str, data){
 	    // Figure out if we're getting a template, or if we need to
 	    // load the template - and be sure to cache the result.
@@ -35,52 +37,68 @@ var T = {
 	    return data ? fn(data) : fn	
 	},
 
+	// render a template with the specific name, load the template if necessary, return rendered contents via callback method
 	renderTemplate : function(name, data, callback){
 		var that = this
 		if(that.cachedTemplates[name] == null) {
 			that.loadTemplate(name, function() {
-				that.renderCachedTemplate(name, data, callback)
+				callback(that.renderCachedTemplate(name, data))
 			})
 	    }else{
-	    	that.renderCachedTemplate(name, data, callback)
+	    	callback(that.renderCachedTemplate(name, data))
 	    }	
 	},
 
-	renderCachedTemplate : function(name, data, callback){
-		callback(this.renderString(this.cachedTemplates[name], data))	
+	// render a template with the specific name from cached templates, return the rendered data
+	renderCachedTemplate : function(name, data){
+		return this.renderString(this.cachedTemplates[name], data)	
 	},
 
+	// render a collection of objects using a template 'name', load the template if necessary, return rendered contents via callback method
 	renderCollection : function(name, collection, callback) {
 		var that = this
 		if(that.cachedTemplates[name] == null) {
 			that.loadTemplate(name, function() {
-				that.renderCachedCollection(name, collection, callback)
+				callback(that.renderCachedCollection(name, collection))
 			})
 	    }else{
-	    	that.renderCachedCollection(name, collection, callback)
+	    	callback(that.renderCachedCollection(name, collection))
 	    }
 	},
 
-	renderCachedCollection : function(name, collection, callback){
+	// render a template and collection from cached templates, return the rendered data
+	renderCachedCollection : function(name, collection){
 		var result = '';
 		for ( var i in collection) {
 			result += this.renderString(this.cachedTemplates[name], collection[i])        	
-		}		
-		callback(result)
+		}	
+		return result;
 	},
 
-	renderIn : function(holder, name, data){   
-	    holder.html(Template.renderCachedTemplate(name, data))
+	// render template in a specific DOM element ('holder') 
+	renderIn : function(holder, name, data, callback){
+		var that = this
+		this.renderTemplate(name, data, function(rendered){
+			holder.html(that.renderCachedTemplate(name, data))
+			callback()
+		})	    
+	},
+	
+	// render a cached template in a specific DOM element ('holder')
+	renderCachedIn : function(holder, name, data){
+		holder.html(this.renderCachedTemplate(name, data))
 	},
 
+	// render a collection in a specific DOM element ('holder')
 	renderCollectionIn : function(holder, name, collection, callback){
-	    holder.each(function(index, element) {
-	        element.innerHTML = ''
-	    })
-
 	    this.renderCollection(name, collection, function(data){
-	    	holder.append(data)
+	    	holder.html(data)
 	    	callback()
 	    })
-	}		
+	},
+	
+	// render a collection in a cached template and add to specific DOM element ('holder')
+	renderCachedCollectionIn : function(holder, name, collection){
+	    holder.html(this.renderCachedCollection(name, collection))
+	}
 }
