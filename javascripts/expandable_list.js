88eecb4b898fae4782b3587040f90202e6b17b04
bjosip <josip.biscan@gmail.com>
2010-08-04 10:56:33 +0200

    modified list contructor so it can be extended without errors

--- javascripts/expandable_list.js
+++ javascripts/expandable_list.js
@@ -1,18 +1,18 @@
 /**
- * A list of elements where only one can be current (focused) at a given time, and when it's current it is in
- * the "expanded" state
+ * A list of elements where only one can be current (focused) at a given time,
+ * and when it's current it is in the "expanded" state
  */
 
-//extend(ExpandableList, List)
+extend(ExpandableList, List)
 
-function ExpandableList(options){
+function ExpandableList(options) {
   console.log(this.constructor)
 }
 
-ExpandableList.prototype.constructor = function(){
-  	
+ExpandableList.prototype.constructor = function() {
+
 }
 
-ExpandableList.prototype.setCurrent = function(item){
+ExpandableList.prototype.setCurrent = function(item) {
   // TODO
 }
--- javascripts/list.js
+++ javascripts/list.js
@@ -1,30 +1,33 @@
 /**
  * A list of elements where only one can be current (focused) at a given time
  */
-function List(options){
-	var that = this
-	
-	if(!options.element){
-		throw "No element specified for list"
-	}
-	
-	this.element = options.element
-	this.items = this.element.find('a')
-	
-	this.current = $(this.items[0])
-	this.clickCallback = options.onClick
-	
-	this.items.bindClick(function(element){
-		that.items.removeClass('current')
-		$(element).addClass('current')
-		if(that.clickCallback){
-			that.clickCallback($(element))
-		}
-	})
-	
+
+function List(options) {
+  var that = this
+
+  if (typeof (options) != 'undefined') {
+    if (!options.element) {
+      throw "No element specified for list"
+    }
+
+    this.element = options.element
+    this.items = this.element.find('a')
+
+    this.current = $(this.items[0])
+    this.clickCallback = options.onClick
+
+    this.items.bindClick(function(element) {
+      that.items.removeClass('current')
+      $(element).addClass('current')
+      if (that.clickCallback) {
+        that.clickCallback($(element))
+      }
+    })
+
     this.current.addClass('current')
+  }
 }
 
-List.prototype.setCurrent = function(item){
-	// TODO
+List.prototype.setCurrent = function(item) {
+  // TODO
 }
