d50f864b12ef965382a94406680898f07309def8
bjosip <josip.biscan@gmail.com>
2010-08-31 16:22:12 +0200

    some repairs on Gps class

--- javascripts/gps.js
+++ javascripts/gps.js
@@ -1,42 +1,35 @@
 var Gps = {
   so : null,
 
-  timeouts : [ 15000000, 60000000, 300000000 ],
+  timeouts : [ 15000000, 60000000, 150000000 ],
 
   timeout : 0,
 
-  position : {
-    latitude : null,
-    longitude : null
-  },
-
   init : function() {
     try {
-      this.so = device.getServiceObject('Service.Location', 'ILocation')
+      Gps.so = device.getServiceObject('Service.Location', 'ILocation')
     } catch (e) {
-      this.so = null
+      Gps.so = null
     }
   },
 
   getLocation : function(callback) {
-    var that = this
-
     try {
-      this.so.ILocation.GetLocation( {
+      Gps.so.ILocation.GetLocation( {
         'LocationInformationClass' : 'BasicLocationInformation',
         'Updateoptions' : {
-          'UpdateTimeOut' : this.timeouts[this.timeout]
-        },
-        'PartialUpdates' : false
+          'UpdateTimeOut' : Gps.timeouts[Gps.timeout],
+          'PartialUpdates' : false
+        }
       }, function(transId, eventCode, result) {
         if (eventCode == 4) {
-          if (++that.timeout >= that.timeouts.length) {
+          if (++Gps.timeout >= Gps.timeouts.length) {
             callback( {
               'error' : 'Timeout'
             })
-            that.timeout = 0
+            Gps.timeout = 0
           } else {
-            that.getLocation(callback)
+            Gps.getLocation(callback)
           }
         } else {
           callback( {
@@ -45,7 +38,7 @@ var Gps = {
         }
       })
     } catch (e) {
-      //Utility.log('GetLocation e: ' + e)
+//      Utility.log('Gps.getLocation exception e:' + e)
     }
   }
 }
