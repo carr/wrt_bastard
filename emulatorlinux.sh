db9f6e92a3ae07e2bc9e537bd86976e2f5cd4f8e
Tomislav Car <tomislav@infinum.hr>
2010-10-10 16:23:04 +0200

    Emulator starter for linux

---
+++ emulatorlinux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# TODO should work for any user, could add chrome folder to path or a better solution?
+/opt/google/chrome/google-chrome "`pwd`/wrt_bastard/emulator.html" --disable-web-security &
